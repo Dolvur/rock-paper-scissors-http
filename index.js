6c9735c9ac437d3b7fa78aa4ef8864c20d7eae35
Daniel Karlsson <danier1999@gmail.com>
2023-01-04 21:42:52 +0100

    added keys for better authorization. Prevent move change after game is done

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const Hapi = require('@hapi/hapi');
 let games = {
     // <gameId>: {
         // player1: {
+            // key : <string | null (not joined)>,
             // name: <string>,
             // move: <rock | paper | scissors | null (not chosen)>
         // },
         // player2: {
+            // key : <string | null (not joined)>,
             // name: <string>,
             // move: <rock | paper | scissors | null (not chosen)>
         // },
@@ -28,12 +30,37 @@ server.route({
     path: '/api/games/{id}',
     handler: function (request, h) {
         const id = request.params.id;
-        if (!games[id]) {
+        const key = request.headers.authorization;
+        const game = games[id];
+        if (!game) {
             return `Game ${id} not found`;
         }
 
-        // return the game state
-        return games[id];
+        const player1 = game.player1;
+        const player2 = game.player2;
+        let visibleState = {
+            player1: {
+                name: player1.name,
+            },
+            player2: {
+                name: player2.name,
+            },
+            winner: game.winner
+        }
+
+        // if the game is over, show both players moves
+        if (game.winner) {
+            visibleState.player1.move = player1.move;
+            visibleState.player2.move = player2.move;
+        }
+        // if the game is not over, only show the player's own move
+        else if (player1.key === key) {
+            visibleState.player1.move = player1.move;
+        } else if (player2.key === key) {
+            visibleState.player2.move = player2.move;
+        }
+
+        return visibleState;
     },
 });
 
@@ -44,18 +71,20 @@ server.route({
     handler: function (request, h) {
         let id = Math.random().toString(36).substring(2, 15);
         const playerName = request.payload.name ? request.payload.name : "player1";
-        while (games[id]) { // make sure the id is unique
+        // make sure the id is unique
+        while (games[id]) { 
             id = Math.random().toString(36).substring(2, 15);
         }
 
+        let player1Key = Math.random().toString(36).substring(2, 15);
         console.log("Creating game with id: " + id);
         games[id] = {
-            player1: {name: playerName, move: null},
-            player2: {name: null, move: null},
+            player1: {key: player1Key, name: playerName, move: null},
+            player2: {key: null, name: null, move: null},
             winner: null
         };
 
-        return "Created game with id: " + id;
+        return `Created game with id: ${id}, player name: ${playerName}, player key: ${player1Key}`
     }
 });
 
@@ -77,7 +106,14 @@ server.route({
         }
 
         games[id].player2.name = playerName;
-        return `Successfully joined game: "${id}", player name: ${playerName}`;
+        let player2Key = Math.random().toString(36).substring(2, 15);
+        // make sure the key is unique
+        while (games[id].player1.key === player2Key) {
+            player2Key = Math.random().toString(36).substring(2, 15);
+        }
+        games[id].player2.key = player2Key;
+
+        return `Successfully joined game: "${id}", player name: ${playerName}, player key: ${player2Key}`;
     }
 });
 
@@ -87,25 +123,32 @@ server.route({
     path: '/api/games/{id}/move',
     handler: function (request, h) {
         const id = request.params.id;
-        const playerName = request.payload.name;
+        const key = request.headers.authorization;
         const validMoves = ["rock", "paper", "scissors"];
         const move = request.payload.move.toLowerCase();
+        const game = games[id];
         
-        if (!games[id]) {
+        if (!game) {
             return `Game ${id} not found`;
+        }
+        if (game.winner) {
+            return `Game ${id} is already over`;
         }
         if (!validMoves.includes(move)) {
             return `Invalid move: ${move}, valid moves are: ${validMoves}`;
         }
         
-        const player1 = games[id].player1;
-        const player2 = games[id].player2;
-        if (player1.name === playerName) {
+        const player1 = game.player1;
+        const player2 = game.player2;
+        let playerName = null;
+        if (player1.key === key) {
             player1.move = move;
-        } else if (player2.name === playerName) {
+            playerName = player1.name;
+        } else if (player2.key === key) {
             player2.move = move;
+            playerName = player2.name;
         } else {
-            return `Player ${playerName} not found in game, available players: ${player1.name}, ${player2.name}`;
+            return `Key: "${key}" invalid for game: ${id}`;
         }
 
         // check if both players have made a move
@@ -122,7 +165,7 @@ server.route({
             }
         }
 
-        return `Move ${move} made by ${playerName} in game ${id}`;
+        return `Action: "${move}" by ${playerName} in game ${id}`;
     }
 });
 
